d7d07f56f0ade949dea38b2738771cc16efaefb5
Robert Burger <robert.burger@dlr.de>
2026-06-23 12:53:38 +0200

    conan: add layout

--- conanfile.py
+++ conanfile.py
@@ -13,19 +13,44 @@ class MainProject(ConanFile):
     description = "Helpers for links-and-nodes support."
     exports_sources = ["*", "!.gitignore" ]
     settings = "os", "compiler", "build_type", "arch"
+    libs = [ 'ln_helper' ]
 
     def requirements(self):
         self.requires("yaml-cpp/0.7.0@3rdparty/stable", transitive_headers=True, transitive_libs=True)
 
-    def source(self):
-        self.run(f"sed 's/AC_INIT(.*/AC_INIT([robotkernel], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
+    def layout(self):
+        self.folders.source = "."
+        self.folders.build = "build"
+        self.folders.generators = "conan"
+
+        # In the local folder (when the package is in development, or "editable") the artifacts can be found:
+        self.cpp.source.includedirs = ["include", "build/include"]
+        self.cpp.build.bindirs = ["src"]
+        self.cpp.build.libdirs = ["src/.libs"]
+        self.cpp.build.libs = self.libs
+
+        # In the Conan cache, we packaged everything at the default standard directories
+        self.cpp.package.libs = self.libs
+        self.cpp.package.bindirs = ["bin"]
+
+        self.layouts.package.runenv_info.append_path("PATH", "bin")
 
     def generate(self):
         tc = AutotoolsToolchain(self)
         tc.generate()
 
     def build(self):
-        self.run("autoreconf -if")
+        configure_ac_in = os.path.join(self.source_folder, "configure.ac.in")
+        if os.path.exists(configure_ac_in):
+            configure_ac = os.path.join(self.source_folder, "configure.ac")
+            self.run(
+                f'sed "s|PACKAGE_VERSION|{self.version}|" configure.ac.in > configure.ac',
+                cwd=self.source_folder
+            )
+            print(f"[INFO] Created configure.ac from configure.ac.in")
+        else:
+            print(f"[WARNING] configure.ac.in not found at {configure_ac_in}")
+
         autotools = Autotools(self)
         autotools.libs=[]
         autotools.include_paths=[]
@@ -34,7 +59,8 @@ class MainProject(ConanFile):
             autotools.flags = ["-O0", "-g", "-fno-builtin-strlen"]
         else:
             autotools.flags = ["-O2"]
-        autotools.configure()
+        autotools.autoreconf(args = [ self.source_folder, ])
+        autotools.configure(build_script_folder=self.source_folder)
         autotools.make()
 
     def package(self):
